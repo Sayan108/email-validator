6b1247e3d0592d16df8d1616b378c0ac5c482e9a
Sayan <sayan.dey@e-arc.com>
2025-10-22 17:35:23 +0530

    Refactor email validation service and controller; reintroduce routes and types

--- app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import bodyParser from "body-parser";
-import emailRoutes from "./routes/emailvalidator.routes";
-
-const app = express();
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use("/validate", emailRoutes);
-
-export default app;
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import emailRoutes from "./routes/emailvalidator.routes";
+import emailRoutes from "./src/routes/emailvalidator.routes";
 
 const app = express();
 app.use(bodyParser.json({ limit: "50mb" }));
